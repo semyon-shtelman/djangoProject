0a623d7cb6a972964a2457d4a0055be559bd2c4f
Semyon Shtelman <shtelmaan@gmail.com>
2026-03-02 04:14:21 +0600

    Fix home page routing

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,6 @@ from catalog.apps import CatalogConfig
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts')
 ]
