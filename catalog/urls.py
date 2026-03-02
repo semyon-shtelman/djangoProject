f5db98a7695e488ddc21d0f79d194bb067125a8b
Semyon Shtelman <shtelmaan@gmail.com>
2026-03-02 20:19:14 +0600

    configure url paths for catalog app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
-from catalog.apps import CatalogConfig
 
-app_name = CatalogConfig.name
+app_name = 'catalog'
 
 urlpatterns = [
     path('', views.home, name='home'),
