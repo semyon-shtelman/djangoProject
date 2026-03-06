ec8528bb2bc5802cbbe4e39181ec40a603878a02
Semyon Shtelman <shtelmaan@gmail.com>
2026-03-07 01:11:32 +0600

    setup admin interface for product and category models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'purchase_price', 'category')
+    list_filter = ('category',)
+    search_fields = ('title', 'description')
