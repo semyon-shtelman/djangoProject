81dd803e48da502454e40c1f70eb7c3f9fc5e085
Semyon Shtelman <shtelmaan@gmail.com>
2026-03-08 18:44:15 +0600

    create a custom command to add test products

---
+++ catalog/management/commands/add_products.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+    help = 'Add test products to the database'
+
+    def handle(self, *args, **kwargs):
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+        call_command('loaddata', 'catalog/fixtures/categories_fixture.json')
+        call_command('loaddata', 'catalog/fixtures/products_fixture.json')
+
+        self.stdout.write(self.style.SUCCESS(f'loaded categories {Category.objects.count()}'))
+        self.stdout.write(self.style.SUCCESS(f'loaded products {Product.objects.count()}'))
