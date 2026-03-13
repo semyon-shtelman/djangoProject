0f08f165626edc544385eaa28e14f5bce3d6bc24
Semyon Shtelman <shtelmaan@gmail.com>
2026-03-14 01:16:08 +0600

    add a path to the product detail page

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,6 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts')
+    path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail')
 ]
