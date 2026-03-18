c9e9802547b927c6305f7f8c052c3f6b047d3da3
Semyon Shtelman <shtelmaan@gmail.com>
2026-03-18 21:09:15 +0600

    refactoer: convert views from FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'catalog'
+app_name = "catalog"
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('products/<int:pk>/', views.product_detail, name='product_detail')
+    path("", views.HomeListView.as_view(), name="home"),
+    path("contacts/", views.ContactsTemplateView.as_view(), name="contacts"),
+    path(
+        "products/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"
+    ),
 ]
