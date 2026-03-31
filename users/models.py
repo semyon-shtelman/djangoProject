38ecb3aabe2617b1e8f24e6e0bc5d83ba49dcee7
Semyon Shtelman <shtelmaan@gmail.com>
2026-03-31 22:23:23 +0600

    user model has been created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email', help_text='Укажите вашу почту')
+    phone = models.CharField(max_length=15, blank=True, null=True, verbose_name='Номер телефона', help_text='Введите номер телефона')
+    avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True, verbose_name='Аватар', help_text='Загрузите свой аватар')
+    country = models.CharField(max_length=100, blank=True, null=True, verbose_name='Страна', )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
