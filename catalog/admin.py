d58a2af3f8d17cbbfe1513fdad79cd046ea39886
Semyon Shtelman <shtelmaan@gmail.com>
2026-04-08 13:29:25 +0600

    update admin panel for Product

--- catalog/admin.py
+++ catalog/admin.py
@@ -13,6 +13,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "price", "category")
+    list_display = ("id", "title", "price", "category", 'owner')
     list_filter = ("category",)
     search_fields = ("title", "description")
