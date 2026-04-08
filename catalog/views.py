a9ab5ef51c3d69981d5e66bd95487ac3e9c15328
Semyon Shtelman <shtelmaan@gmail.com>
2026-04-08 13:28:24 +0600

    add permission checks to views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,10 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
+from functools import update_wrapper
+from itertools import product
+
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -13,11 +18,29 @@ class ProductCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy("catalog:product_list")
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
 
 class ProductListView(generic.ListView):
     model = Product
     template_name = "catalog/product_list.html"
     context_object_name = "products"
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if not user.is_authenticated:
+            return Product.objects.filter(is_publish=True)
+
+        if user.has_perm('catalog.can_unpublish_product'):
+            return Product.objects.all()
+
+        published_product = Product.objects.filter(is_publish=True)
+        my_product = Product.objects.filter(owner=user)
+
+        return published_product.union(my_product)
 
 
 class ProductDetailView(LoginRequiredMixin, generic.DetailView):
@@ -25,20 +48,51 @@ class ProductDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "catalog/product_detail.html"
     context_object_name = "product"
 
+    def get_object(self):
+        product = get_object_or_404(Product, pk=self.kwargs['pk'])
+        user = self.request.user
 
-class ProductUpdateView(LoginRequiredMixin, generic.UpdateView):
+        if user.has_perm('catalog.can_unpublish_product'):
+            return product
+
+        if user == product.owner:
+            return product
+
+        if product.is_publish:
+            return product
+
+        raise Http404("Продукт не найден или недоступен")
+
+class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     model = Product
     form_class = ProductForm
     template_name = "catalog/product_form.html"
+
+    def test_func(self):
+        product = self.get_object()
+        user = self.request.user
+
+        if user.has_perm('catalog.can_unpublish_product'):
+            return True
+        return user == product.owner
+
 
     def get_success_url(self):
         return reverse_lazy("catalog:product_detail", kwargs={"pk": self.object.pk})
 
 
-class ProductDeleteView(LoginRequiredMixin, generic.DeleteView):
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
     model = Product
     template_name = "catalog/product_confirm_delete.html"
     success_url = reverse_lazy("catalog:product_list")
+
+    def test_func(self):
+        product = self.get_object()
+        user = self.request.user
+
+        if user.has_perm('catalog.can_unpublish_product'):
+            return True
+        return user == product.owner
 
 
 class ContactsTemplateView(generic.TemplateView):
